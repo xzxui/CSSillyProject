ebefd27eb666cfe52b8a18dbe6a1d427ca4d975c
xzxui <jhuisheng60@gmail.com>
2025-10-25 14:47:06 +0800

    added path to excel of testing history to description of that module

--- ModuleCreateExcelOfTestingHistory.py
+++ ModuleCreateExcelOfTestingHistory.py
@@ -7,5 +7,5 @@ def CreateExcelOfTestingHistory():
     Return:
         No return
     Process:
-        Create an excel file about the student's testing history, showing the score and grade achieved in each paper, and the AI's comment on the performance of the student. Notice that the path of the folder containing all the student's practice paper history is stored in configs.marking_result_folder. Every single file ending with 'xlsx' under that folder should be a marking result save.
+        Create an excel file about the student's testing history, showing the score and grade achieved in each paper, and the AI's comment on the performance of the student, and save to configs.path_to_excel_of_testing_history. Notice that the path of the folder containing all the student's practice paper history is stored in configs.marking_result_folder. Every single file ending with 'xlsx' under that folder should be a marking result save.
     """
