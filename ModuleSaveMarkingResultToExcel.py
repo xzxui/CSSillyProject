e4e14e2ac4bd47ec1cc9212179a5b7e211d9236a
EuropaQWQ <3264407356@qq.com>
2025-10-26 11:46:27 +0800

    Updated moduleSaveMarkingResultToExcel.py

--- ModuleSaveMarkingResultToExcel.py
+++ ModuleSaveMarkingResultToExcel.py
@@ -1,4 +1,5 @@
 import configs
+import pandas as pd
 
 def SaveMarkingResultToExcel(save_path, syllabus_code, component_number, marking_report, strengths, weaknesses, score, grade):
     """
@@ -15,4 +16,14 @@ def SaveMarkingResultToExcel(save_path, syllabus_code, component_number, marking
     Process:
         Save the marking result into an excel file, and call ModuleCreateExcelOfTestingHistory.CreateExcelOfTestingHistory
     """
+    Marking_report_columns = ['Question Number', 'Maximum Mark Possible', 'Marks Received']
+    df_report = pd.DataFrame(marking_report, columns= Marking_report_columns)
+    df_paper = pd.DataFrame({'Syllabus Code': [syllabus_code], 'Component Number': [component_number]})
+    df_feekback = pd.DataFrame({'Weaknesses': [weaknesses], 'Strengths': [strengths]})
+    df_result = pd.DataFrame({'Score':[score], 'Grade':[grade]})
+
+    df_sheet=pd.concat([df_paper, df_report,df_result, df_feekback], axis=1)
+    with pd.ExcelWriter(save_path, engine='openpyxl') as writer:
+        df_sheet.to_excel(writer, sheet_name='Marking Report', index=False)
     pass
+#test: SaveMarkingResultToExcel('test_folder\\1.xlsx','syllabus', 'component' ,  [['1', 5, 4], ['2(a)', 10, 4], ['2(b)(i)', 5,4], ['2(b)(ii)',5,5]] , 'good', 'bad',114,514)
